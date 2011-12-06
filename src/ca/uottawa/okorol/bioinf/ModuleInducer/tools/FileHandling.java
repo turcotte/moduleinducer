d55dcbdb980cbea8fdbb0e929fb793b3569c2263
okoro103 <okoro103@localhost>
2011-12-06 18:38:22 -0500

    Changes for Web commit 1.006: new style (minor)

--- src/ca/uottawa/okorol/bioinf/ModuleInducer/tools/FileHandling.java
+++ src/ca/uottawa/okorol/bioinf/ModuleInducer/tools/FileHandling.java
@@ -174,9 +174,23 @@ public class FileHandling {
 	
 	
 	private static String getHTMLResultsHeader(){
-		String str = "<html><head>	<title>Module Inducer: Results</title>	<meta http-equiv=\"Refresh\" content=\"5\" /></head><body><div style=\"border-style:solid; border-width:1px; border-color: #348017;  font-family: Verdana, Geneva, sans-serif; text-align: center; font-size: 9pt;\">" +
-				"	<h2 style=\"text-align: center;  font-family: Verdana, Geneva, sans-serif; font-size: 13pt;\">Module Inducer</h2>	Extract knowledge from biological data.<br/>	Oksana Korol and Marcel Turcotte, University of Ottawa	<br/>&nbsp;</div>	" +
-				"<div style=\"margin: 2em 2em 2em 2em; font-family: Verdana, Geneva, sans-serif; \"><pre>";
+		String str = "<html><head> \n\n" +
+				"<title>Module Inducer: Results</title>	<meta http-equiv=\"Refresh\" content=\"5\" />\n\n" +
+				"<style> <!--\n" +
+				"		* {font-family: Verdana, Geneva, sans-serif; font-size: 9pt; }\n" +
+				"		.main {width: 700px; margin: 25px 25px 25px 25px; }\n" +
+				"		.title {font-size: 13pt; font-weight:bold;	}\n" +
+				"		.header {background-color: white; padding: 10px; margin: 10px; border-style:solid; border-width:2px; border-color: #348017; text-align: center; }\n" +
+				"		.mi_results_body {background-color: white; padding: 10px; margin: 10px;}\n" +
+				"--> </style>\n\n" +
+				"</head>\n\n" +
+				"<body>\n\n" +
+				"<div class=\"main\">\n" +
+				"<div class=\"header\"> <div class=\"title\">ModuleInducer ::</div>:: results </div>\n" +
+				"<br/>\n" +
+				"<div class=\"mi_results_body\">\n" +
+				"<pre>\n";
+
 		
 		return str;
 	}
@@ -188,14 +202,17 @@ public class FileHandling {
 				"to more than a day, if the dataset is large and/or motifs need to be discovered. <br/><br/>" +
 				"Please save the link to this page to return to it later. <br/><br/>" +
 				"Note: if the results do not appear in more than a day, your data might be too <br/>" +
-				"large to run through the web interface. Please contact okoro103 at uottawa.ca or <br/>" +
-				"turcotte at eecs.uottawa.ca to arrange for an off-line run.<br/>";
+				"large to run through the web interface. Please contact Module.Inducer@site.uOttawa.ca <br/>" +
+				"to arrange for an off-line run.<br/>";
 		
 		return str;
 	}
 	
 	private static String getHTMLResultsFooter(){
-		String str = "</pre></div></body></html>";
+		String str = "</pre>\n" +
+				"</div>\n" +
+				"</div>\n" +
+				"</body></html>";
 		
 		return str;
 	}
